ea062d62a7f62284f73981f34ed3befe89499aa4
pleblira <91032628+pleblira@users.noreply.github.com>
2022-10-02 18:27:17 -0400

    Moving assets to their specific folders, and adding more images for LNcap

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 import urllib.request
 import text_on_images
 import subprocess
+import os
 
 sched = BlockingScheduler()
 
@@ -56,17 +57,17 @@ def LN_flippening_tracker():
     )
 
     # asking if would like to choose image or pick a random one
-    random_image_or_choose = input("Pick and image or choose at random (pick/random)? ")
-    if random_image_or_choose == "pick":
+    random_image_or_pick = input("Pick and image or choose at random (pick/random)? ")
+    if random_image_or_pick == "pick":
         image_url_or_path = input("insert image full URL or path here: ")
         if image_url_or_path.find("http")>-1 and image_url_or_path.find("//")>0:
             urllib.request.urlretrieve(image_url_or_path, "assets/00000001.jpg")
             tweet_image = "assets/00000001.jpg"
         else:
             tweet_image = image_url_or_path
-    elif random_image_or_choose == "random":
+    elif random_image_or_pick == "random":
         random_image_picker = random.randint(1,5)
-        tweet_image = "assets/" + str(random_image_picker) + ".png"
+        tweet_image = "assets/full_belly/" + str(random_image_picker) + ".png"
     else:
         print("Wrong option detected")
         quit()
@@ -86,6 +87,8 @@ def LN_flippening_tracker():
     if confirm_send_tweet == "y":
         tweepy_send_tweet(tweet_message,tweet_image)
         print("Tweet sent")
+        if random_image_or_pick == "pick" and image_url_or_path.find("http")>-1 and image_url_or_path.find("//")>0:
+            os.remove("assets/00000001.jpg")
         quit()
     else:
         return
@@ -95,24 +98,30 @@ def LN_flippening_tracker():
 def LN_cap():
     # fetching LN capacity in BTC
     LN_capacity_in_BTC = amboss_get_LN_capacity()
-    LN_capacity_text = "Current LN channel capacity: " + str(LN_capacity_in_BTC) + "BTC"
+    # LN_capacity_text = "Current LN channel capacity: " + str(LN_capacity_in_BTC) + "BTC"
+    LN_capacity_text = "Current LN channel capacity: " + str(5000) + "BTC"
 
     # picking random image
-    random_image_picker = random.randint(1,5)
-    tweet_image = str(random_image_picker) + ".png"
+    # random_image_picker = random.randint(1,5)
+    # tweet_image = "assets/blank_belly_dark_mode/" + str(random_image_picker) + ".jpg"
+    tweet_image = "assets/blank_belly_dark_mode/2.jpg"
 
     # typing LN capacity on mascot
-    text_on_images.image_draw_angled(LN_capacity_in_BTC)
+    # tweet_image = text_on_images.image_draw_angled(LN_capacity_in_BTC, tweet_image)
+    tweet_image = text_on_images.image_draw_angled(5000, tweet_image)
+    subprocess.call(('open', "assets/tweet_image.jpg"))
 
     # LIGHTNING NETWORK CAPACITY TWEET
+    tweet_message = (
+    "LIGHTNING NETWORK CAPACITY UPDATE" + "\n\n" + 
+    LN_capacity_text + "\n"
+        )
+    print(tweet_message)
     confirm_send_tweet = input("Send tweet (y/n)? ")
     if confirm_send_tweet == "y":
-        tweet_message = (
-        "LIGHTNING NETWORK CAPACITY UPDATE" + "\n\n" + 
-        LN_capacity_text + "\n"
-            )
-        tweepy_send_tweet(tweet_message,tweet_image)
+        tweepy_send_tweet(tweet_message,"assets/tweet_image.jpg")
         print("Tweet sent")
+        os.remove("assets/tweet_image.jpg")
         quit()
     else:
         return
--- text_on_images.py
+++ text_on_images.py
@@ -1,18 +1,51 @@
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 import os
 
-def image_draw_angled(LN_capacity_in_BTC):
-        img = Image.open("assets/mascot_belly.jpg")
-        font = ImageFont.truetype("SourceCodePro-Bold.ttf",74)
-        
-        # Creating a temporary canvas, drawing the text on it, and rotating
-        temporary_canvas = Image.new(mode='L', size=(500,500))
-        text_on_temporary_canvas = ImageDraw.Draw(temporary_canvas)
-        text_on_temporary_canvas.text((0, 0), str(LN_capacity_in_BTC) + "\nBTC", font=font, fill=255, align="center")
-        rotated_text_on_temporary_canvas=temporary_canvas.rotate(-24, resample=3, expand=1)
+# todo configuration for images 1, 3, 4, 5.jpg - 2.jpg and 6.jpg are done and good to go
 
-        # pasting temporary canvas on main image
-        img.paste( ImageOps.colorize(rotated_text_on_temporary_canvas, (0,0,0), (242,169,0)), (222,895), rotated_text_on_temporary_canvas)
 
-        img.show()
-        return img
+def image_draw_angled(LN_capacity_in_BTC, tweet_image):
+
+        if tweet_image == "assets/blank_belly_dark_mode/2.jpg":
+                tweet_image = Image.open(tweet_image)
+                # font = ImageFont.truetype("SourceCodePro-Bold.ttf",100)
+                font = ImageFont.truetype("Silom.ttf",95)
+                
+                # Creating a temporary canvas, drawing the text on it, and rotating
+                temporary_canvas = Image.new(mode='L', size=(500,500))
+                text_on_temporary_canvas = ImageDraw.Draw(temporary_canvas)
+                text_on_temporary_canvas.text((0, 0), str(LN_capacity_in_BTC) + "\nBTC", font=font, fill=255, align="center")
+                # rotated_text_on_temporary_canvas=temporary_canvas.rotate(-24, resample=3, expand=1)
+                rotated_text_on_temporary_canvas=temporary_canvas.rotate(-2.5, resample=3, expand=1)
+
+                # pasting temporary canvas on main image
+                # tweet_image.paste( ImageOps.colorize(rotated_text_on_temporary_canvas, (0,0,0), (242,169,0)), (462,1190), rotated_text_on_temporary_canvas)
+                tweet_image.paste( ImageOps.colorize(rotated_text_on_temporary_canvas, (0,0,0), (242,169,0)), (712,1315), rotated_text_on_temporary_canvas)
+
+                # tweet_image.show()
+                tweet_image.save("assets/tweet_image.jpg")
+                return None
+                
+        if tweet_image == "assets/blank_belly_dark_mode/6.jpg":
+                tweet_image = Image.open(tweet_image)
+                # font = ImageFont.truetype("SourceCodePro-Bold.ttf",100)
+                font = ImageFont.truetype("Silom.ttf",90)
+                
+                # Creating a temporary canvas, drawing the text on it, and rotating
+                temporary_canvas = Image.new(mode='L', size=(500,500))
+                text_on_temporary_canvas = ImageDraw.Draw(temporary_canvas)
+                text_on_temporary_canvas.text((0, 0), str(LN_capacity_in_BTC) + "\nBTC", font=font, fill=255, align="center")
+                # rotated_text_on_temporary_canvas=temporary_canvas.rotate(-24, resample=3, expand=1)
+                rotated_text_on_temporary_canvas=temporary_canvas.rotate(-23.2, resample=3, expand=1)
+
+                # pasting temporary canvas on main image
+                # tweet_image.paste( ImageOps.colorize(rotated_text_on_temporary_canvas, (0,0,0), (242,169,0)), (462,1190), rotated_text_on_temporary_canvas)
+                tweet_image.paste( ImageOps.colorize(rotated_text_on_temporary_canvas, (0,0,0), (242,169,0)), (455,1190), rotated_text_on_temporary_canvas)
+
+                # tweet_image.show()
+                tweet_image.save("assets/tweet_image.jpg")
+                return None
+
+
+# if __name__ == "__main__":
+#     image_draw_angled(5002, "assets/blank_belly_dark_mode/6.jpg")
